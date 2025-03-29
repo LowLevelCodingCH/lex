fab69ecccb9affd4f8f92645fff053e1f564a5a4
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2025-03-29 12:21:56 +0100

    Use TAR

--- include/lex/lex.h
+++ include/lex/lex.h
@@ -23,34 +23,36 @@ void lex_rbase(void)
 
 void lex_rebase(int argc, char **argv)
 {
-    char *fname = argv[2];
-    char *flag;
+    char *flag = "NONE";
     char *message = "NONE";
-    char *rbs = (char*) malloc(1);
+    char *rbs = (char*) malloc(100);
 
-    if (argc == 5) flag = argv[3];
-    if (argc == 5) message = argv[4];
+    if (argc == 4) {
+        flag = argv[2];
+        message = argv[3];
+        free(rbs);
+        rbs = (char*) malloc(strlen(message) + strlen("REBASED") + strlen("Message: ") + 2);
+        sprintf(rbs, "REBASED\nMessage: %s", message);
+    }
 
     if (strcmp(flag, "NONE") != 0 && strcmp(flag, "--message") != 0) {
         fprintf(stderr, "No valid flag found\n");
         return;
     }
 
-    char *ename = (char*) malloc((strlen(fname) + strlen(".lex")) * 2);
-    if (argc == 5) rbs = (char*) malloc(strlen(message) + strlen(fname) + strlen("REBASED: ") + strlen("Message: ") + 2);
+    system("tar cf snapshot.tar --exclude=.lex --exclude=snapshot.tar .");
 
-    int fd = open(fname, O_RDONLY);
+    int fd = open("snapshot.tar", O_RDONLY);
+
     off_t f_len = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
     char *buffer = (char*) malloc(f_len * 2 + 1);
     read(fd, buffer, f_len);
+
     close(fd);
 
-    sprintf(ename, ".lex/%s", fname);
-    if (strcmp(flag, "--message") == 0) sprintf(rbs, "REBASED: %s\nMessage: %s", fname, message);
-
-    fd = open(ename, O_CREAT | O_RDWR);
-    chmod(ename, 0777);
+    fd = open(".lex/snapshot.tar", O_CREAT | O_RDWR);
+    chmod(".lex/snapshot.tar", 0777);
     write(fd, buffer, f_len);
     close(fd);
 
@@ -59,14 +61,13 @@ void lex_rebase(int argc, char **argv)
     close(fd);
 
     free(buffer);
-    free(ename);
     free(rbs);
 }
 
 void lex_init(void)
 {
     mkdir(".lex", 0777);
-    close(open(".lex/BASE", O_CREAT, S_IRUSR | S_IWUSR));
+    open(".lex/BASE", O_CREAT, S_IRUSR | S_IWUSR);
     chmod(".lex/BASE", 0777);
     printf("Initialized LEX dir (.lex)\n");
 }
--- src/main.c
+++ src/main.c
@@ -16,17 +16,15 @@ int main(int argc,
     switch (argc) {
     case 2:
         if (strcmp(argv[1], "rbase") == 0)
-            lex_rbase(); break;
+            lex_rbase();
         if (strcmp(argv[1], "init") == 0)
             lex_init();
-        else return fprintf(stderr, "No valid command: %s\n", argv[1]);
-        break;
-    case 4:
-    case 5:
         if (strcmp(argv[1], "rebase") == 0)
             lex_rebase(argc, argv);
-        else return fprintf(stderr, "No valid command: %s\n", argv[1]);
         break;
+    case 4:
+        if (strcmp(argv[1], "rebase") == 0)
+            lex_rebase(argc, argv);
     }
     return 0;
 }
